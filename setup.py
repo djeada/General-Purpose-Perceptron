c1aac76597b99ebf318b8134659901f05137febc
djeada <addjellouli1@gmail.com>
2024-02-18 11:09:32 +0100

    [infra]: improve and fill setup.py file

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 from setuptools import find_packages, setup
 
+version = "0.1"
+
 setup(
-    name="neura_command",  # Use underscores instead of hyphens
-    version="0.1",
+    name="neura_command",
+    version=version,
     packages=find_packages("src"),
     package_dir={"": "src"},
-    install_requires=[
-        "numpy>=1.25.2",
-    ],
-    # Additional metadata
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A short description of your project",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",  # If your README is Markdown
-    url="https://github.com/yourusername/neura_command",
+
+    # Use a requirements file for better dependency management
+    install_requires=open("requirements.txt").readlines(),
+
+    # Metadata about the package
+    author="Adam Djellouli",
+    author_email="addjellouli1@gmail.com",
+    description="NeuraCommand is a simplistic tool for creating, training, and deploying neural networks. It provides a seamless interface for handling complex neural network architectures, including perceptrons with multiple layers. With NeuraCommand, users can efficiently load data, train models, and use them for predictions.",
+
+    # Use context manager for reading the long description
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/djeada/NeuraCommand",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    # Add other metadata as needed
+    # Add additional metadata as needed
 )
